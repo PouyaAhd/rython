e0d010ef0a85e833cee534f014da0151520166a7
pouya <ahadipooya@gmail.com>
2023-01-04 15:04:17 +0330

    added seen check and button to see each case

--- pages/cases/index.jsx
+++ pages/cases/index.jsx
@@ -1,82 +1,126 @@
-import React, { useState } from "react";
-import { Container, Row, Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Card, Col } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import styles from "./index.module.css";
 import Collapse from "react-bootstrap/Collapse";
 import Button from "react-bootstrap/Button";
+import { BsCheckAll } from "react-icons/bs";
 
 function index() {
   const [toggle, setToggle] = useState(0);
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState();
+  const [lengths, setLeangths] = useState([]);
+  const [fire, setFire] = useState([])
+  const [vehicle, setVehicle] = useState([])
+  const [responsibility, setResponsibility] = useState([])
+  const [engineering, setEngineering] = useState([])
   const [objectsList, setObjectsList] = useState([
     {
       id: 1,
       erjaDate: "1401/10/11",
       lateDate: "1401/10/12",
       name1: "عرفانیان",
-      activity:"کافی شاپ",
+      activity: "کافی شاپ",
       name2: "آتش سوزی-غیر صنعتی",
       name3: "پارسیان",
-      type: "آتش سوزی"
+      type: "آتش سوزی",
+      immediate: true
     },
     {
       id: 2,
       erjaDate: "1401/10/11",
       lateDate: "1401/10/12",
       name1: "عرفانیان",
+      activity: "کافی شاپ",
       name2: "آتش سوزی-صنعتی",
       name3: "پارسیان",
-      type: "آتش سوزی"
+      type: "آتش سوزی",
+      immediate: false
     },
     {
       id: 3,
       erjaDate: "1401/10/11",
       lateDate: "1401/10/12",
       name1: "عرفانیان",
+      activity: "کافی شاپ",
       name2: "مسئولیت-تمام خطر اموال",
       name3: "پارسیان",
-      type: "مسئولیت"
+      type: "مسئولیت",
+      immediate: true
     },
     {
       id: 4,
       erjaDate: "1401/10/11",
       lateDate: "1401/10/12",
       name1: "عرفانیان",
+      activity: "کافی شاپ",
       name2: "اتوموبیل-سواری",
       name3: "پارسیان",
-      type: "خودرو"
+      type: "خودرو",
+      immediate: false
     },
     {
       id: 5,
       erjaDate: "1401/10/11",
       lateDate: "1401/10/12",
       name1: "عرفانیان",
+      activity: "کافی شاپ",
       name2: "اتوموبیل-سواری",
       name3: "پارسیان",
-      type: "خودرو"
+      type: "خودرو",
+      immediate: true
     },
     {
       id: 6,
       erjaDate: "1401/10/11",
       lateDate: "1401/10/12",
       name1: "عرفانیان",
+      activity: "کافی شاپ",
       name2: "مهندسی-شکست ماشین آلات",
       name3: "پارسیان",
-      type: "مهندسی"
+      type: "مهندسی",
+      immediate: true
     },
     {
       id: 7,
       erjaDate: "1401/10/11",
       lateDate: "1401/10/12",
       name1: "عرفانیان",
+      activity: "کافی شاپ",
       name2: "مهندسی-شکست ماشین آلات",
       name3: "پارسیان",
-      type: "مهندسی"
+      type: "مهندسی",
+      immediate: true
     }
   ]);
 
+  useEffect(() => {
+    
+    setFire(objectsList.filter(e=>e.type === 'آتش سوزی'))
+   
+    setVehicle(objectsList.filter(e=>e.type === 'خودرو'))
+    setResponsibility(objectsList.filter(e=>e.type === 'مسئولیت'))
+    setEngineering(objectsList.filter(e=>e.type === 'مهندسی'))
+    
+  }, [fire,vehicle,responsibility,engineering]);
+
   return (
     <Container fluid>
+      <Row className="text-center mt-2">
+        <Col xs={6}>
+          <div>{fire.length}:آتش سوزی</div>
+        </Col>
+        <Col xs={6}>
+          <div>{vehicle.length}:اتوموبیل</div>
+        </Col>
+        <Col className="mt-2" xs={6}>
+          <div>مسئولیت:{responsibility.length}</div>
+        </Col>
+        <Col className="mt-2" xs={6}>
+          <div>مهندسی:{engineering.length}</div>
+        </Col>
+      </Row>
+      <hr />
       <Table
         className={styles.container}
         style={{ direction: "rtl" }}
@@ -189,14 +233,12 @@ function index() {
         <Card className="border-0">
           <div style={{ direction: "rtl", cursor: "pointer" }}>
             {objectsList.map((item, index) => (
-              <div
-                onClick={() => {
-                  setToggle(item.id);
-                  toggle === index + 1 && setToggle(!toggle);
-                }}
-                className="border m-2  rounded"
-              >
+              <div className="border m-2  rounded">
                 <div
+                  onClick={() => {
+                    setToggle(item.id);
+                    toggle === index + 1 && setToggle(!toggle);
+                  }}
                   className="p-2"
                   style={
                     item.type === "آتش سوزی"
@@ -211,7 +253,9 @@ function index() {
                   }
                 >
                   <div className="d-flex justify-content-between">
-                    <h5 style={{ width: "100%",fontSize:"18px" }}>تاریخ ارجاع</h5>
+                    <h5 style={{ width: "100%", fontSize: "18px" }}>
+                      تاریخ ارجاع
+                    </h5>
                     <div style={{ width: "100%" }}>
                       {" "}
                       <p style={{ fontWeight: "bold", textAlign: "right" }}>
@@ -222,7 +266,9 @@ function index() {
                     <div></div>
                   </div>
                   <div className="d-flex justify-content-between">
-                    <h5 style={{ width: "100%",fontSize:"18px" }}>بازدید با تاخیر</h5>
+                    <h5 style={{ width: "100%", fontSize: "18px" }}>
+                      بازدید با تاخیر
+                    </h5>
                     <div style={{ width: "100%" }}>
                       {" "}
                       <p style={{ fontWeight: "bold", textAlign: "right" }}>
@@ -232,7 +278,9 @@ function index() {
                     <div></div>
                   </div>
                   <div className="d-flex justify-content-between">
-                    <h5 style={{ width: "100%",fontSize:"18px" }}>بیمه گذار</h5>
+                    <h5 style={{ width: "100%", fontSize: "18px" }}>
+                      بیمه گذار
+                    </h5>
                     <p
                       style={{
                         fontWeight: "bold",
@@ -245,7 +293,9 @@ function index() {
                     <div></div>
                   </div>
                   <div className="d-flex justify-content-between">
-                    <h5 style={{ width: "100%",fontSize:"18px" }}>بیمه نامه</h5>
+                    <h5 style={{ width: "100%", fontSize: "18px" }}>
+                      بیمه نامه
+                    </h5>
                     <p
                       style={{
                         fontWeight: "bold",
@@ -258,7 +308,7 @@ function index() {
                     <div></div>
                   </div>
                   <div className="d-flex justify-content-between">
-                    <h5 style={{ width: "100%",fontSize:"18px" }}>بیمه گر</h5>
+                    <h5 style={{ width: "100%", fontSize: "18px" }}>بیمه گر</h5>
                     <p
                       style={{
                         fontWeight: "bold",
@@ -270,13 +320,19 @@ function index() {
                     </p>
                     <div></div>
                   </div>
+                  {item.immediate && (
+                    <div>
+                      <BsCheckAll
+                        style={{ fontSize: "22px", color: "#2ECC71" }}
+                      />
+                    </div>
+                  )}
                 </div>
                 <Collapse in={toggle === index + 1 ? true : false}>
                   <div
                     className="border rounded"
                     style={{
                       direction: "rtl",
-                      cursor: "pointer",
                       padding: "0",
                       marginTop: "5px"
                     }}
@@ -287,14 +343,13 @@ function index() {
                       <p>4888</p>
                     </div>
                     <hr style={{ width: "95%" }} className=" m-auto" />
-                    
-                    
+
                     <div className=" d-flex justify-content-between p-2">
                       <h5>شعبه</h5>
                       <p>مشهد</p>
                     </div>
                     <hr style={{ width: "95%" }} className=" m-auto" />
-                    
+
                     <div className=" d-flex justify-content-between p-2">
                       <h5>شماره گزارش</h5>
                       <p>2339-آت-1401</p>
@@ -304,7 +359,7 @@ function index() {
                       <h5>شهر</h5>
                       <p>مشهد</p>
                     </div>
-                   
+
                     <hr style={{ width: "95%" }} className=" m-auto" />
                     <div className=" d-flex justify-content-between p-2">
                       <h5>کارشناس بازدید</h5>
@@ -314,6 +369,9 @@ function index() {
                     <div className=" d-flex justify-content-between p-2">
                       <h5>وضعیت</h5>
                       <p>بازدید شد-در حال تهیه گذارش</p>
+                    </div>
+                    <div className="p-2">
+                      <Button className="w-100 p-10">مشاهده پرونده</Button>
                     </div>
                   </div>
                 </Collapse>
